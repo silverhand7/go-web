026f8cbcfbd708162d0cf1e1a67bfa9f96545dc8
silverhand7 <refojunior5@gmail.com>
2023-02-16 16:30:13 +0800

    learning golang web

---
+++ handler_test.go
@@ -0,0 +1,25 @@
+package go_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// kita bisa menggunakan writer.Write() tapi membutuhkan parameter byte,
+		// maka kita bisa menggunakan package fmt (format) untuk membantu
+		fmt.Fprint(writer, "Hello World")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
