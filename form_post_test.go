ffa3ddc45527315b438fca613672d4cc5867930c
silverhand7 <refojunior5@gmail.com>
2023-02-18 20:59:48 +0800

    learn form post

---
+++ form_post_test.go
@@ -0,0 +1,40 @@
+package go_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	firstName := request.PostForm.Get("first_name")
+	lastName := request.PostForm.Get("last_name")
+
+	// atau bisa langsung menggunakan code dibawah
+	// request.PostFormValue("first_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first_name=Danaerys&last_name=Targaryen")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
