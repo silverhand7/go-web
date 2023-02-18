485c310fd8127ec9254b0b85bc5cfba696ae71bc
silverhand7 <refojunior5@gmail.com>
2023-02-18 20:51:28 +0800

    learn go web

---
+++ header_test.go
@@ -0,0 +1,50 @@
+package go_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// Request Header
+func RequestHeader(writer http.ResponseWriter, request *http.Request) {
+	contentType := request.Header.Get("content-type")
+	fmt.Fprint(writer, contentType)
+}
+
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", nil)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+// Response Header
+func ResponseHeader(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("X-Powered-By", "Lorem Ipsum")
+	fmt.Fprint(writer, "OK")
+}
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", nil)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	ResponseHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+	fmt.Println(response.Header.Get("X-Powered-By"))
+}
